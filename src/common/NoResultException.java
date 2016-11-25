82450506bd5c3e1db42a3a7c0521d8305af11ff2
Simon <simon@simonhuang.ca>
2016-11-24 21:20:23 -0500

    Minor fixes
    Minor code fix.
    Libraries added.
    Release added.

--- src/common/NoResultException.java
+++ src/common/NoResultException.java
@@ -1,6 +1,11 @@
 package common;
 
 public class NoResultException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public NoResultException(){
 		super("No results found!");
 	}
